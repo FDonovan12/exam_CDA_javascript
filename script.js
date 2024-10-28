146538c3560c1c59dbd8a7090c1973ad70e3f8ef
FDonovan12 <donovanferreira12@gmail.com>
2024-10-28 21:15:43 +0100

    shopping cart is functionnal

--- script.js
+++ script.js
@@ -7,17 +7,37 @@ fetch('produits.json')
 
 let fullProducts = [];
 let productsDiv = [];
-let shoppingCart = [];
+let shoppingCart = getShoppingCartLocal();
+// resetShoppingCart();
 
 const searchBar = document.querySelector('#search');
 const priceMin = document.querySelector('#price-min');
 const priceMax = document.querySelector('#price-max');
+const shoppingDiv = document.querySelector('#shopping');
+const shoppingIconDiv = document.querySelector('#shopping-icon');
 const shoppingCartDiv = document.querySelector('#shopping-cart');
-// const priceRange = document.querySelector('#price-range');
+const body = document.querySelector('body');
+
+searchBar.addEventListener('input', () => showProducts(filterProducts()));
+priceMin.addEventListener('input', () => showProducts(filterProducts()));
+priceMax.addEventListener('input', () => showProducts(filterProducts()));
+shoppingIconDiv.addEventListener('click', (event) => openShopping(event));
+body.addEventListener('click', (event) => {
+    if (event.target.matches('body')) {
+        closeAll();
+    }
+});
+
+saveShoppingCart();
 
 function filterProducts() {
     return fullProducts
-        .filter((product) => searchIsvallid(searchBar.value, product.nom_produit))
+        .map((product) => {
+            return { product: product, substring: numberOfSubstringValid(searchBar.value, product.nom_produit) };
+        })
+        .filter((product) => product.substring > 0)
+        .sort((product1, product2) => product2.substring - product1.substring)
+        .map((product) => product.product)
         .filter(
             (product) =>
                 parseFloat(product.prix) >= parseFloat(priceMin.value) &&
@@ -25,35 +45,31 @@ function filterProducts() {
         );
 }
 
-function searchIsvallid(search, name) {
+function numberOfSubstringValid(search, name) {
     const listSearch = slugify(search).split('-');
-    const containSubstrig = listSearch
-        .map((sea) => slugify(name).includes(slugify(sea)))
+    const slugifyName = slugify(name);
+    const numberOfSubstringValid = listSearch
+        .map((sea) => slugifyName.includes(sea))
+        .map((bool) => (bool ? 1 : 0))
         .reduce((acc, sea) => acc + sea);
-    return containSubstrig;
+    return numberOfSubstringValid;
 }
+
 function slugify(str) {
     str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
-    str = str.toLowerCase(); // convert string to lowercase
+    str = str.toLowerCase();
     str = str
-        .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+        .replace(/[^a-z0-9 -]/g, '-') // replace any non-alphanumeric characters by hyphens
         .replace(/\s+/g, '-') // replace spaces with hyphens
         .replace(/-+/g, '-'); // remove consecutive hyphens
     return str;
 }
 
-searchBar.addEventListener('input', () => showProducts(filterProducts()));
-priceMin.addEventListener('input', () => showProducts(filterProducts()));
-priceMax.addEventListener('input', () => showProducts(filterProducts()));
-
 function showProducts(products) {
-    console.log('showProducts');
     const parentDiv = document.querySelector('.products-grid');
     parentDiv.innerHTML = '';
     productsDiv = products.map((product) => showProduct(product, parentDiv));
     cardIsUnselected(parentDiv);
-    console.log(productsDiv);
-    console.log(products);
 }
 
 function showProduct(product, parentDiv) {
@@ -83,10 +99,11 @@ function showProduct(product, parentDiv) {
         `;
 
     detail.querySelector('button').addEventListener('click', () => addToShoppingCart(product));
-    card.addEventListener('click', () => {
-        console.log(card);
-        console.log(card.getAttribute('selected'));
-        if (card.getAttribute('selected') == '') {
+    card.addEventListener('click', (event) => {
+        if (event.target.matches('button')) {
+            return;
+        }
+        if (card.getAttribute('selected') === '') {
             cardIsUnselected(parentDiv, card);
         } else {
             cardIsSelected(parentDiv, card);
@@ -97,19 +114,92 @@ function showProduct(product, parentDiv) {
     return card;
 }
 
-function addToShoppingCart(product) {
-    shoppingCart.push(product);
-    shoppingCartDiv.innerHTML = shoppingCart.map((product) => product.nom_produit);
-}
-
 function cardIsSelected(parentDiv, card) {
-    card.toggleAttribute('selected');
-    card.querySelector('button').setAttribute('tabindex', 0);
+    card.setAttribute('selected', '');
+    card.querySelector('button').setAttribute('tabindex', 1);
     parentDiv.querySelectorAll('.card:not([selected])').forEach((card) => card.setAttribute('tabindex', -1));
 }
 
 function cardIsUnselected(parentDiv, card) {
-    card?.toggleAttribute('selected');
+    card?.removeAttribute('selected');
     card?.querySelector('button').setAttribute('tabindex', -1);
     parentDiv.querySelectorAll('.card').forEach((card) => card.setAttribute('tabindex', 1));
+}
+
+function addToShoppingCart(product) {
+    const filterShop = shoppingCart.filter((shopProduct) => shopProduct.product.id === product.id);
+    if (filterShop.length === 0) {
+        shoppingCart.push({ product: product, number: 1 });
+    } else {
+        filterShop[0].number += 1;
+    }
+    saveShoppingCart();
+}
+
+function addContentToShoppingCart() {
+    shoppingCartDiv.innerHTML = '';
+    shoppingCart.forEach((product) => {
+        div = createProductContentToShoppingCart(product);
+        shoppingCartDiv.appendChild(div);
+    });
+    const total = document.createElement('div');
+    shoppingCartDiv.appendChild(total);
+}
+
+function createProductContentToShoppingCart(product) {
+    console.log('createProductContentToShoppingCart');
+    const lineProduct = document.createElement('div');
+    lineProduct.innerHTML = `
+        <span class="name">${product.product.nom_produit} : </span><span class="number">${product.number}</span>
+        <button name="plus" class="btn-primary fa-solid fa-plus" tabindex="-1"></button>
+        <button name="minus" class="btn-primary fa-solid fa-minus" tabindex="-1"></button>
+    `;
+
+    const plus = lineProduct.querySelector('[name="plus"]');
+    const minus = lineProduct.querySelector('[name="minus"]');
+
+    plus.addEventListener('click', () => {
+        product.number += 1;
+        saveShoppingCart();
+    });
+    minus.addEventListener('click', () => {
+        product.number <= 1 ? removeProductFromShoppingCart(product) : (product.number -= 1);
+        saveShoppingCart();
+    });
+    return lineProduct;
+}
+
+function removeProductFromShoppingCart(product) {
+    console.log(product);
+    console.log(shoppingCart[0]);
+    shoppingCart = shoppingCart.filter((productShop) => product.product.id != productShop.product.id);
+    saveShoppingCart();
+}
+
+function openShopping(event) {
+    if (event.target.matches('button')) {
+        return;
+    }
+    shoppingDiv?.toggleAttribute('opened');
+}
+
+function saveShoppingCart() {
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+    console.log(shoppingCart);
+    addContentToShoppingCart();
+}
+
+function resetShoppingCart() {
+    localStorage.setItem('shoppingCart', []);
+    shoppingCart = getShoppingCartLocal();
+}
+
+function getShoppingCartLocal() {
+    const local = localStorage.getItem('shoppingCart');
+    return local ? JSON.parse(local) : [];
+}
+
+function closeAll() {
+    productsDiv.map((productDiv) => cardIsUnselected(body, productDiv));
+    shoppingDiv?.toggleAttribute('opened');
 }
